271b7e9f8a9ac42c68f089d27233eeb894870898
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2023-05-08 02:16:56 +0900

    [Silver III] Title: 비슷한 단어, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/2607. 비슷한 단어/비슷한 단어.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+N= int(input())
+target = list(input().rstrip()) # 첫 단어 (비교 대상)
+answer = 0
+
+for _ in range(N-1):
+    compare = target[:] 
+    word = input().rstrip()     # 새로운 단어
+    cnt = 0
+
+    for w in word:
+        if w in compare:
+            compare.remove(w)
+        else:
+            cnt += 1
+
+    if cnt < 2 and len(compare) < 2:
+        answer += 1
+
+print(answer)

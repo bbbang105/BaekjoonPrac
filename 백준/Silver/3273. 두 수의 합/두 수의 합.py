2ec85689bb16fc9c6b5ec84e741a6368a02ae509
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2023-05-24 11:50:45 +0900

    [Silver III] Title: 두 수의 합, Time: 152 ms, Memory: 126320 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,29 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-def BinarySearch(start):
-    global cnt, lst
-    index = len(lst)-1
-    while start != lst[index]:
-        sumNum = start + lst[index]
-        if sumNum == goal:
-            cnt += 1
-            lst = lst[:index]
-            break
-        elif sumNum < goal:
-            break
-        index -= 1
-    return
-            
-if __name__ == '__main__':
-    N = int(input())
-    lst = sorted(list(map(int,input().rstrip().split())))
-    goal = int(input())
-    
-    cnt = 0
-    for i in lst:
-        if i not in lst:
-            break
-        BinarySearch(i)
+N = int(input())
+lst = sorted(list(map(int,input().rstrip().split())))
+goal = int(input())
+left,right = 0, len(lst)-1
+cnt = 0
+
+while left != right:
+    temp = lst[left] + lst[right]
+    if temp == goal:
+        cnt += 1
+    if temp < goal:
+        left += 1
+    else:
+        right -= 1
         
-    print(cnt)
+print(cnt)

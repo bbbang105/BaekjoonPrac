3a385a7671a114941cfd5aa6d5619a21bdd34c63
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2023-08-01 19:26:09 +0900

    [Gold IV] Title: 연구소, Time: 648 ms, Memory: 117604 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -4,6 +4,8 @@ from itertools import combinations
 input = sys.stdin.readline
 
 def bfs(x,y):
+    global del_cnt
+    
     Q = deque()
     Q.append((x,y))
     visited[x][y] = True
@@ -18,20 +20,20 @@ def bfs(x,y):
                 # 바이러스 퍼뜨리기
                 if graph[nx][ny] == 0 and not visited[nx][ny]:
                     visited[nx][ny] = True
-                    del_lst.append((nx,ny))
+                    del_cnt += 1
                     Q.append((nx,ny))
     
 N,M = map(int,input().split())
 graph = [list(map(int,input().split())) for _ in range(N)]
 
 # 초기 빈 칸, 바이러스 위치 파악
-safe_lst,birus_lst = [],[]
+safe_lst,virus_lst = [],[]
 for i in range(N):
     for j in range(M):
         if graph[i][j] == 0:
             safe_lst.append((i,j))
         elif graph[i][j] == 2:
-            birus_lst.append((i,j))
+            virus_lst.append((i,j))
             
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
@@ -39,11 +41,11 @@ dy = [0,0,-1,1]
 res = 0
 for wall_lst in combinations(safe_lst,3):
     visited = [[False]*M for _ in range(N)]
-    del_lst = []
+    del_cnt = 0
     # 바이러스 위치만으로 bfs 실행
-    for x,y in birus_lst:
+    for x,y in virus_lst:
         bfs(x,y)
     # 안전영역 = (초기 빈 칸 개수 - 바이러스에 감염된 개수 - 벽의 개수 3)
-    res = max(res, len(safe_lst) - len(del_lst) - 3)
+    res = max(res, len(safe_lst) - del_cnt - 3)
     
 print(res)

440a2c90cc188518a95482497b59a80481e82bfe
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2023-09-11 18:21:44 +0900

    [unrated] Title: 할 일 목록, Time: 0.03 ms, Memory: 75.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181885. 할 일 목록/할 일 목록.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] todo_list, boolean[] finished) {
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < todo_list.length; i++) {
+            if (finished[i] != true) {
+                list.add(todo_list[i]);
+            }
+        }
+        return list.toArray(new String[list.size()]);
+    }
+}

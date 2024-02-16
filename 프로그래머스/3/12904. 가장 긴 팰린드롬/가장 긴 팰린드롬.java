e2eb91298f8ee76b63c82fa187a714026953a8e8
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2024-02-17 02:35:18 +0900

    [level 3] Title: 가장 긴 팰린드롬, Time: 0.25 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
+++ 프로그래머스/3/12904. 가장 긴 팰린드롬/가장 긴 팰린드롬.java
@@ -4,14 +4,14 @@ class Solution {
         int answer = 1;
 
         for (int i = 0; i < len; i++) {
-            for (int j = len - 1; j >= 0; j--) {
+            for (int j = len - 1; j >= i + answer - 1; j--) { // 현재 팰린드롬 길이보다 작은 경우는 볼 필요 x
                 int curLength = j - i + 1;
-                if (answer >= curLength) break;
                 if (s.charAt(i) == s.charAt(j)) {
                     boolean flag = true;
-                    int rightIndex = j;
-                    for (int k = i; k < j + 1 ; k++) {
-                        if (s.charAt(k) != s.charAt(rightIndex--)) {
+                    int leftIndex = i + 1; 
+                    int rightIndex = j - 1; 
+                    while (leftIndex < rightIndex) { 
+                        if (s.charAt(leftIndex++) != s.charAt(rightIndex--)) {
                             flag = false;
                             break;
                         }

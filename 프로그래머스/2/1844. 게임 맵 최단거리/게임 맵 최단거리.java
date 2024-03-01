26a8e7a3efd5717e76921854226f5b8fc330ca62
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2024-03-02 00:33:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.15 ms, Memory: 52.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,42 +1,42 @@
 import java.util.*;
 
 class Solution {
-    private int answer;
-    private int[][] graph;
-    private int n, m;
-    private boolean[][] visited;
-    private final int[] dx = {-1, 1, 0, 0};
-    private final int[] dy = {0, 0, -1, 1};
+    private static final int[] DX = {-1, 1, 0, 0};
+    private static final int[] DY = {0, 0, -1, 1};
+    private static int answer;
     
     public int solution(int[][] maps) {
         answer = -1;
-        graph = maps;
-        n = graph.length; m = graph[0].length;
-        visited = new boolean[n][m];
-        bfs();
+        bfs(maps, new Node(0, 0, 1));
         
         return answer;
     }
     
-    private void bfs() {
+    private void bfs(int[][] graph, Node node) {
+        int row = graph.length;
+        int col = graph[0].length;
+        
+        boolean[][] visited = new boolean[row][col];
+        visited[node.x][node.y] = true;
         Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(0, 0, 1));
-        visited[0][0] = true;
+        q.offer(node);
         
         while(!q.isEmpty()) {
-            Node node = q.poll();
-            if (node.x == n - 1 && node.y == m - 1) {
-                answer = node.cnt;
+            Node cur = q.poll();
+            
+            if (cur.x == row - 1 && cur.y == col - 1) {
+                answer = cur.cnt;
                 break;
             }
             
             for (int i = 0; i < 4; i++) {
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (graph[nx][ny] == 1 && !visited[nx][ny]) {
-                    q.offer(new Node(nx, ny, node.cnt + 1));
-                    visited[nx][ny] = true;
+                int x = cur.x + DX[i];
+                int y = cur.y + DY[i];
+                
+                if (x < 0 || x >= row || y < 0 || y >= col) continue;
+                if (!visited[x][y] && graph[x][y] != 0) {
+                    visited[x][y] = true;
+                    q.offer(new Node(x, y, cur.cnt + 1));
                 }
             }
         }
@@ -47,7 +47,7 @@ class Solution {
         int y;
         int cnt;
         
-        Node(int x, int y, int cnt) {
+        Node (int x, int y, int cnt) {
             this.x = x;
             this.y = y;
             this.cnt = cnt;

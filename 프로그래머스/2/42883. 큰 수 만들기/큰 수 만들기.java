3fc097b13eb5bd0277a024746a7ca0456bc48a46
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2024-03-01 16:22:33 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.04 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,15 +1,17 @@
+import java.util.*;
+
 class Solution {
     public String solution(String number, int k) {
         StringBuilder sb = new StringBuilder(number);
-        int startIndex = 0;
-
+        int start = 0;
+        
         for (int i = 0; i < k; i++) {
-            for (int j = startIndex; j < sb.length() - 1; j++) {
-                if (sb.charAt(startIndex) == '9') {
-                    startIndex++;
+            for (int j = start; j < sb.length() - 1; j++) {
+                if (sb.charAt(j) == '9') {
+                    start++;
                     continue;
                 }
-
+                
                 int n1 = sb.charAt(j) - '0';
                 int n2 = sb.charAt(j + 1) - '0';
 
@@ -20,12 +22,7 @@ class Solution {
             }
         }
         
-        String answer = sb.toString();
-        
-        if (answer.length() > number.length() - k) {
-            answer = answer.substring(0, number.length() - k);
-        }
-        
-        return answer;
+        // 동일한 숫자가 이어져, 마지막에 최소값들이 남았을 경우
+        return sb.toString().substring(0, number.length() - k);
     }
 }

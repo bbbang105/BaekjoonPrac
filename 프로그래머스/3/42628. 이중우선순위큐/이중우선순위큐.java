317768e310a5bdb668a1afd2c08b8d6e01a20ace
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2024-03-01 23:18:34 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.48 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,43 +1,31 @@
-import java.util.Collections;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
-        PriorityQueue<Integer> min = new PriorityQueue<>();
-        StringTokenizer st;
-        int[] answer;
-        int delNum;
-
+        int[] answer = new int[2];
+        Queue<Integer> min = new PriorityQueue<>();
+        Queue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+        
         for (String operation : operations) {
-            st = new StringTokenizer(operation);
-            char op = st.nextToken().charAt(0);
-            int num = Integer.parseInt(st.nextToken());
-
-            switch (op) {
-                case 'I' :
-                    max.offer(num);
-                    min.offer(num);
-                    break;
-                case 'D':
-                    if (max.isEmpty() || min.isEmpty()) break;
-                    if (num == 1) {
-                        delNum = max.poll();
-                        min.remove(delNum);
-                    } else if (num == -1) {
-                        delNum = min.poll();
-                        max.remove(delNum);
-                    }
+            String[] temp = operation.split(" ");
+            String cmd = temp[0];
+            String num = temp[1];
+            
+            if (cmd.equals("I")) {
+                min.offer(Integer.parseInt(num));
+                max.offer(Integer.parseInt(num));
+            } else if (!min.isEmpty() && num.equals("1")) {
+                int n = max.poll();
+                min.remove(n);
+            } else if (!min.isEmpty() && num.equals("-1")){
+                int n = min.poll();
+                max.remove(n);  
             }
         }
-
-        if (max.isEmpty() || min.isEmpty()) {
-            answer = new int[]{0,0};
-        } else {
-            answer = new int[]{max.peek(), min.peek()};
-        }
-
+        
+        if (!max.isEmpty()) answer[0] = max.poll(); 
+        if (!max.isEmpty()) answer[1] = min.poll();
+        
         return answer;
     }
 }

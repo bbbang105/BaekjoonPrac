178f543a231503d420d3ab57751be9828e1c72c0
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2024-03-02 00:50:14 +0900

    [level 3] Title: 입국심사, Time: 0.35 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -2,34 +2,30 @@ import java.util.*;
 
 class Solution {
     public long solution(int n, int[] times) {
-        long result = 0;
-        int len = times.length;
-        
+        long answer = 0;
         Arrays.sort(times);
         
-        long minTime = 1;
-        long maxTime = (long) times[len - 1] * n; // 최대로 걸리는 시간
+        long left = 1;
+        long right = (long) times[times.length - 1] * n; // 최대로 소요되는 시간
         
-        while (minTime <= maxTime) {
-            long avgTime = (long) (minTime + maxTime) / 2;
-            long possibleCnt = 0;
+        while (left <= right) {
+            long mid = (long) (left + right) / 2;
+            long cnt = 0;
             
             for (int t : times) {
-                possibleCnt += (avgTime / t); 
+                cnt += mid / t;
                 
-                if (possibleCnt >= n) break;
+                if (cnt >= n) break;
             }
             
-            if (possibleCnt < n) {
-                // 심사가 불가한 경우
-                minTime = avgTime + 1;
+            if (cnt >= n) {
+                answer = mid;
+                right = mid - 1;
             } else {
-                // 심사가 가능한 경우, 최적 시간 갱신
-                result = avgTime;
-                maxTime = avgTime - 1;
+                left = mid + 1;
             }
         }
         
-        return result;
+        return answer;
     }
 }

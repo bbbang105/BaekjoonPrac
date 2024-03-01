678357653997b2e5c9f976ebbf0c9006a5aa9def
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2024-03-01 23:51:11 +0900

    [level 3] Title: 등굣길, Time: 0.77 ms, Memory: 53 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,26 +1,28 @@
+import java.util.*;
+
 class Solution {
-    private final static int MOD = 1000000007;
+    private static final int MOD = 1_000_000_007;
     
     public int solution(int m, int n, int[][] puddles) {
-        int[][] dp = new int[n + 1][m + 1];
-        dp[1][1] = 1; // 시작점
+        int row = n; int col = m;
+        int[][] dp = new int[row][col];
         
         for (int[] puddle : puddles) {
-            dp[puddle[1]][puddle[0]] = -1;
+            dp[puddle[1] - 1][puddle[0] - 1] = -1;
         }
         
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                // 시작점이거나 웅덩이인 경우 건너뜀
-                if ((i == 1 && j == 1) || dp[i][j] == -1) continue;
+        dp[0][0] = 1;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (i == 0 && j == 0) continue;
+                if (dp[i][j] == -1) continue;
                 
-                // 위쪽에서 오는 경로와 왼쪽에서 오는 경로의 합을 MOD로 나눈 나머지로 설정
-                int up = (i > 1 && dp[i-1][j] != -1 ? dp[i-1][j] : 0);
-                int left = (j > 1 && dp[i][j-1] != -1 ? dp[i][j-1] : 0);
+                int up = (i >= 1 && dp[i - 1][j] != -1 ? dp[i-1][j] : 0);
+                int left = (j >= 1 && dp[i][j - 1] != -1 ? dp[i][j - 1] : 0);
                 dp[i][j] = (up + left) % MOD;
             }
         }
-        
-        return dp[n][m];
+
+        return dp[row - 1][col - 1];
     }
 }

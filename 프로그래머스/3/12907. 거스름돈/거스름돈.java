216fe8760c7166cb95344e376d608f8a74455e3d
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2024-03-24 19:00:19 +0900

    [level 3] Title: 거스름돈, Time: 11.76 ms, Memory: 52.6 MB -BaekjoonHub

---
+++ 프로그래머스/3/12907. 거스름돈/거스름돈.java
@@ -0,0 +1,16 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+    
+    public int solution(int n, int[] money) {
+        int[] answer = new int[100001];
+        answer[0] = 1;
+        
+        for (int i : money) {
+            for (int j = i; j <= n; j++) {
+                answer[j] += answer[j - i];
+            }
+        }
+        
+        return answer[n] % MOD;
+    }
+}

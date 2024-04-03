0a3025326f75229987f50e5ba9e7a00323808e1a
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2024-04-03 11:05:28 +0900

    [Bronze II] Title: 진법 변환, Time: 124 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String N = st.nextToken();
+        int B = Integer.parseInt(st.nextToken());
+        br.close();
+
+        int temp = 1; // B진수의 몇 승인지
+        int sum = 0; // 10진수의 값
+
+        // 맨 오른쪽부터 확인
+        for (int i = N.length() - 1; i >= 0; i--) {
+            Character c = N.charAt(i);
+
+            if (!Character.isDigit(c)) {
+                sum += (c - 55) * temp;
+            } else {
+                sum += (c - '0') * temp;
+            }
+
+            temp *= B; // 1승씩 +
+        }
+
+        System.out.println(sum);
+    }
+}

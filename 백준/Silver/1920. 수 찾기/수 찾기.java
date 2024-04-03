f69e9954fcdb72b95c0e7a83d6b5892d98537c85
SanghoHan <113084292+bbbang105@users.noreply.github.com>
2024-04-03 11:11:56 +0900

    [Silver IV] Title: 수 찾기, Time: 816 ms, Memory: 47156 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+import static java.util.Collections.binarySearch;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        List<Integer> A = new ArrayList<>();
+
+        while (st.hasMoreTokens()) {
+            A.add(Integer.parseInt(st.nextToken()));
+        }
+
+        Collections.sort(A);
+
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        while (st2.hasMoreTokens()) {
+            int num = Integer.parseInt(st2.nextToken());
+
+            int id = binarySearch(A, num); // 있다면 0이상
+
+            if (id >= 0) {
+                sb.append(1).append('\n');
+            } else {
+                sb.append(0).append('\n');
+            }
+        }
+
+        System.out.println(sb.toString());
+    }
+}

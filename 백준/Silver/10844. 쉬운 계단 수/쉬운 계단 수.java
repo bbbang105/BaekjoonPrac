8cd530aa88cb89e77bcc9434e68549b6e4d9e095
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2024-04-22 16:51:02 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 124 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+public class Main {
+    static final int MOD = 1_000_000_000;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        long[][] dp = new long[N + 1][10];
+
+        // 첫 번째 자릿수 (=1의 자리)는 모두 1로 초기화
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        // i는 자릿수를 의미함
+        for (int i = 2; i <= N; i++) {
+            // j는 i번째 자릿수의 자릿값을 의미함 (0 ~ 9)
+            for (int j = 0; j < 10; j++) {
+
+                // 0인 경우에는 옆 자리가 1인 경우에만 가능
+                if (j == 0) {
+                    dp[i][j] = dp[i - 1][1] % MOD;
+                }
+                // 9인 경우에는 옆 자리가 8인 경우에만 가능
+                else if (j == 9) {
+                    dp[i][j] = dp[i-1][8] % MOD;
+                }
+                // 그 외는 본인 +- 1까지 가능
+                else {
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+                }
+            }
+        }
+
+        // 최종 값 계산
+        long result = 0;
+        for (int i = 1; i < 10; i++) {
+            result += (dp[N][i] % MOD);
+        }
+
+        System.out.println(result % MOD);
+    }
+}

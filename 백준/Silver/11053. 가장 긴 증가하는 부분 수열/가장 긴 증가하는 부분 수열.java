1e06649b7621e6216bd389e817aeec803c20c849
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2024-04-22 11:43:03 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 140 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -7,14 +7,13 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
+        int[] dp = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
+            dp[i] = 1;
         }
-
-        int[] dp = new int[N];
-        Arrays.fill(dp, 1); // 최소 길이 1로 초기화
 
         for (int i = 0; i < N; i++) {
             int cur = arr[i];

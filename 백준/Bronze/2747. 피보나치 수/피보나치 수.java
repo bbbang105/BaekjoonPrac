2a47be42ad4e03e3e4d818c89244bc1f69207c70
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2024-04-22 11:39:14 +0900

    [Bronze II] Title: 피보나치 수, Time: 124 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,6 +1,4 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,12 +11,10 @@ public class Main {
         dp[0] = 0;
         dp[1] = 1;
 
-        if (N > 1) {
-            for (int i = 2; i <= N; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2];
-            }
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-
+        
         System.out.println(dp[N]);
     }
 }

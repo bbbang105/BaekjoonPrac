ea078f5f5aa2e62e14475929311982a1bbc74068
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2024-05-11 13:45:14 +0900

    [Silver III] Title: 바이러스, Time: 124 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int answer = 0;
+    private static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 1; i <= N; i++) {
+            map.put(i, new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            // 양방향
+            map.get(v).add(e);
+            map.get(e).add(v);
+        }
+        visited = new boolean[N + 1];
+        dfs(1, map);
+
+        System.out.println(answer);
+    }
+
+    private static void dfs(int cur, Map<Integer, List<Integer>> map) {
+        visited[cur] = true;
+
+        for (int next : map.get(cur)) {
+            if (!visited[next]) {
+                answer++;
+                dfs(next, map);
+            }
+        }
+    }
+}

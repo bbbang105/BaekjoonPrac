8b27e3551533f6c56fa388a33dfdce5932b2d50a
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2024-10-08 10:17:02 +0900

    [Silver III] Title: 파일 정리, Time: 772 ms, Memory: 64072 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Map<String, Integer> map = new HashMap<>();
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String fileName = br.readLine();
+            String extension = fileName.split("\\.")[1];
+            map.put(extension, map.getOrDefault(extension, 0) + 1);
+        }
+
+        List<String> keys = new ArrayList<>(map.keySet());
+        Collections.sort(keys);
+
+        StringBuilder sb = new StringBuilder();
+        for (String key : keys) {
+            int count = map.get(key);
+            sb.append(key).append(" ").append(count).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+}

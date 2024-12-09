e2b0bcba4d85f5e290faf67a08f7608a78e9b801
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2024-12-09 20:14:33 +0900

    [Silver V] Title: 문서 검색, Time: 100 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/1543. 문서 검색/문서 검색.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String originWord = br.readLine();
+        String findWord = br.readLine();
+        int len = findWord.length();
+
+        int idx = 0;
+        int cnt = 0;
+        while (idx <= originWord.length() - len) {
+            String curWord = originWord.substring(idx, idx + len);
+            if (curWord.equals(findWord)) {
+                cnt++;
+                idx += len;
+            } else {
+                idx++;
+            }
+        }
+
+        System.out.print(cnt);
+    }
+}

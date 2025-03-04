5f6f8e376ea779cb05c1fe026007ab672babe638
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-03-05 02:10:36 +0900

    [Silver I] Title: 쿼드트리, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Silver/1992. 쿼드트리/쿼드트리.java
+++ 백준/Silver/1992. 쿼드트리/쿼드트리.java
@@ -1,57 +1,60 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    private static int N;
-    private static int[][] graph;
 
-    private static StringBuilder sb;
+    private static StringBuilder result;
+    private static String line;
+    private static int size;
+    private static int[][] board;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        N = Integer.parseInt(br.readLine());
-        graph = new int[N][N];
-
-        for (int i = 0; i < N; i++) {
-            String row = br.readLine();
-            for (int j = 0; j < N; j++) {
-                graph[i][j] = row.charAt(j) - '0';
+        size = Integer.parseInt(br.readLine());
+        board = new int[size][size];
+        for (int rowIndex = 0; rowIndex < size; rowIndex++) {
+            line = br.readLine();
+            for (int colIndex = 0; colIndex < size; colIndex++) {
+                board[rowIndex][colIndex] = line.charAt(colIndex) - '0';
             }
         }
 
-        sb = new StringBuilder();
-        divideAndConquer(N, 0, 0);
-
-        System.out.println(sb);
+        result = new StringBuilder();
+        compress(0, 0, size); // 처음에는 전 배열을 탐색
+        System.out.println(result);
     }
 
-    private static void divideAndConquer(int n, int x, int y) {
-        int startNum = graph[x][y];
+    private static void compress(int startX, int startY, int moveCount) {
+        int first = board[startX][startY];
+        int nextMoveCount;
+        boolean isAllSame = true;
 
-        // n x n 영역에서 값이 모두 같은지 확인
-        boolean isSame = true;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (graph[x + i][y + j] != startNum) {
-                    isSame = false;
+        for (int rowIndex = 0; rowIndex < moveCount; rowIndex++) {
+            for (int colIndex = 0; colIndex < moveCount; colIndex++) {
+                if (board[startX + rowIndex][startY + colIndex] != first) {
+                    isAllSame = false;
                     break;
                 }
             }
-            if (!isSame) break;
+            if (!isAllSame) {
+                break;
+            }
         }
 
-        // 모든 값이 같다면 해당 숫자 추가
-        if (isSame) {
-            sb.append(startNum);
+        if (!isAllSame) {
+            // 모든 값이 같지 않다면
+            result.append("(");
+            nextMoveCount = moveCount / 2;
+            // 범위를 반으로 줄여 4분할로 다시 탐색
+            compress(startX, startY, nextMoveCount);
+            compress(startX, startY + nextMoveCount, nextMoveCount);
+            compress(startX + nextMoveCount, startY, nextMoveCount);
+            compress(startX + nextMoveCount, startY + nextMoveCount, nextMoveCount);
+            result.append(")");
         } else {
-            // 값이 다르면 4등분하여 재귀 호출
-            sb.append('(');
-            int nextInterval = n / 2;
-            divideAndConquer(nextInterval, x, y);
-            divideAndConquer(nextInterval, x, y + nextInterval);
-            divideAndConquer(nextInterval, x + nextInterval, y);
-            divideAndConquer(nextInterval, x + nextInterval, y + nextInterval);
-            sb.append(')');
+            // 모든 값이 같다면 숫자 추가
+            result.append(first);
         }
     }
 }

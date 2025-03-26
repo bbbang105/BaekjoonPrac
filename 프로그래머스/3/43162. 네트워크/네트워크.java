7f656128fea89f0cbb4a7e20ee0969af87068584
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-03-26 09:25:41 +0900

    [level 3] Title: 네트워크, Time: 0.50 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,21 +1,29 @@
 class Solution {
-    private static int[] parent;
+    
+    private static int[] parents;
     
     public int solution(int n, int[][] computers) {
-        parent = new int[n];
-        for (int i = 0; i < n; i++) parent[i] = i;
+        parents = new int[n];
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
+        }
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
                 if (i != j && computers[i][j] == 1) {
-                    if (find(i) != find(j)) union(i, j);
+                    if (find(i) != find(j)) {
+		                    // 부모가 다른 경우
+                        union(i, j);
+                    }
                 }
             }
         }
         
         int answer = 0;
         for (int i = 0; i < n; i++) {
-            if (parent[i] == i) answer++;
+            if (parents[i] == i) {
+                answer++;
+            }
         }
         
         return answer;
@@ -24,12 +32,15 @@ class Solution {
     private void union(int a, int b) {
         int pa = find(a);
         int pb = find(b);
-        
-        if (pa != pb) parent[pb] = pa;
+        parents[pb] = pa; // 한 방향으로 통일해야 함
     }
     
     private int find(int n) {
-        if (parent[n] == n) return n;
-        else return parent[n] = find(parent[n]);
+        if (parents[n] == n) {
+            return n;
+        }
+        else {
+            return parents[n] = find(parents[n]);
+        }
     }
 }

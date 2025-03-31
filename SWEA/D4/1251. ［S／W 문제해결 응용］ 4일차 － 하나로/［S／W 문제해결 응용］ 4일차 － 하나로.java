1f8009ac81fd7e3872c578c5754c4f38f1aea624
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-03-31 16:03:31 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 868 ms, Memory: 103,120 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -2,13 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /**
  * [조건]
- * 1. 모든 섬을 연결해야 한다. (크루스칼 or 프림)
+ * 1. 모든 섬을 연결해야 한다.
  * 2. 환경 부담금 == 환경 부담 세율(E)와 해저터널 길이(L)의 제곱의 곱만큼 지불 / E * L ^ 2
  * 3. 총 환경 부담금을 최소로 지불, N개의 모든 섬을 연결할 수 있는 교통 시스템을 설계하라.
  * 4. 결과는 소수 첫째 자리에서 반올림하여 정수 형태로 출력하라.
@@ -18,18 +18,18 @@ import java.util.StringTokenizer;
  *
  * [풀이]
  * 1. 간선이 따로 주어지지 않기 때문에, 모든 섬과 섬을 우선 연결하여 저장한다.
- * 2. 해당 값을 통해서 크루스칼 알고리즘을 적용한다.
+ * 2. 해당 값을 통해서 프림 알고리즘을 적용한다.
  * 
  */
 public class Solution {
 	
 	private static int countOfIsland;
 	private static double E;
-	private static int[] parents;
 	private static int[] rows;
 	private static int[] cols;
 	private static double result;
-	private static List<Island> islands;
+	private static List<Edge>[] graph;
+	private static boolean[] isVisited;
 	
 	private static BufferedReader br;
 	private static StringTokenizer st;
@@ -41,28 +41,38 @@ public class Solution {
 		int testCaseNum = Integer.parseInt(br.readLine());
 		for (int testCase = 1; testCase <= testCaseNum; testCase++) {
 			init();
-			findResult();
+			prim();
 			sb.append("#").append(testCase).append(" ").append((long) Math.round(result)).append('\n');
 		}
 		System.out.println(sb);
 	}
 	
 	/**
-	 * 결과를 도출하는 메서드.
+	 * 프림 알고리즘을 활용하여 결과를 도출하는 메서드.
 	 */
-	private static void findResult() {
-		int from, to, fromParent, toParent;
+	private static void prim() {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.offer(new Edge(1, 0));
+		
+		Edge edge;
+		int cur;
 		double weight;
-		for (Island island : islands) {
-			from = island.from;
-			to = island.to;
-			weight = island.weight;
+		while (!pq.isEmpty()) {
+			edge = pq.poll();
+			cur = edge.vertex;
+			weight = edge.weight;
 			
-			fromParent = find(from);
-			toParent = find(to);
-			if (fromParent != toParent) {
-				union(fromParent, toParent);
-				result += E * Math.pow(weight, 2);
+			if (isVisited[cur]) {
+				continue;
+			}
+			
+			isVisited[cur] = true;
+			result += E * Math.pow(weight, 2);
+			
+			for (Edge next : graph[cur]) {
+				if (!isVisited[next.vertex]) {
+					pq.offer(next);
+				}
 			}
 		}
 	}
@@ -72,35 +82,34 @@ public class Solution {
 	 */
 	private static void init() throws IOException {
 		countOfIsland = Integer.parseInt(br.readLine());
-		parents = new int[countOfIsland];
-		rows = new int[countOfIsland];
-		cols = new int[countOfIsland];
+		rows = new int[countOfIsland + 1];
+		cols = new int[countOfIsland + 1];
 		result = 0.0;
-		islands = new ArrayList<>();
+		graph = new ArrayList[countOfIsland + 1];
+		for (int index = 1; index <= countOfIsland; index++) {
+			graph[index] = new ArrayList<>();
+		}
+		isVisited = new boolean[countOfIsland + 1];
 		
 		st = new StringTokenizer(br.readLine());
-		for (int colIndex = 0; colIndex < countOfIsland; colIndex++) {
+		for (int colIndex = 1; colIndex <= countOfIsland; colIndex++) {
 			cols[colIndex] = Integer.parseInt(st.nextToken());
 		}
 		st = new StringTokenizer(br.readLine());
-		for (int rowIndex = 0; rowIndex < countOfIsland; rowIndex++) {
+		for (int rowIndex = 1; rowIndex <= countOfIsland; rowIndex++) {
 			rows[rowIndex] = Integer.parseInt(st.nextToken());
 		}
 		
 		E = Double.parseDouble(br.readLine());
 		
-		for (int index = 0; index < parents.length; index++) {
-			parents[index] = index;
-		}
-		
 		double weight;
-		for (int from = 0; from < countOfIsland - 1; from++) {
-			for (int to = from + 1; to < countOfIsland; to++) {
+		for (int from = 1; from < countOfIsland; from++) {
+			for (int to = from + 1; to <= countOfIsland; to++) {
 				weight = calculateEuclideanDistance(rows[from], cols[from], rows[to], cols[to]);
-				islands.add(new Island(from, to, weight));
+				graph[from].add(new Edge(to, weight));
+				graph[to].add(new Edge(from, weight));
 			}
 		}
-		Collections.sort(islands);
 	}
 	
 	/**
@@ -110,30 +119,21 @@ public class Solution {
 	    return Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
 	}
 	
-	private static void union(int aRoot, int bRoot) {
-		parents[bRoot] = aRoot; 
-	}
-	
-	private static int find(int num) {
-		if (parents[num] == num) {
-			return num;
-		}
-		return parents[num] = find(parents[num]);
-	}
-
-	static class Island implements Comparable<Island> {
-		int from;
-		int to;
+	/**
+	 * 간선 정보를 저장하는 클래스.
+	 * 가중치를 기준으로 오름차순 정렬된다. 
+	 */
+	static class Edge implements Comparable<Edge> {
+		int vertex;
 		double weight;
 		
-		Island(int from, int to, double weight) {
-			this.from = from;
-			this.to = to;
+		Edge(int vertex, double weight) {
+			this.vertex = vertex;
 			this.weight = weight;
 		}
 
 		@Override
-		public int compareTo(Island o) {
+		public int compareTo(Edge o) {
 			return Double.compare(this.weight, o.weight);
 		}
 	}

3167a96afe52ec05e718cf314871bba5aecfbc92
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-03-31 14:47:58 +0900

    [Gold IV] Title: 네트워크 연결, Time: 432 ms, Memory: 50892 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,86 +1,103 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 /**
  * [조건]
- * 1. 모든 컴퓨터를 연결하고, 연결하는 비용을 최소로 하라. (=크루스칼 or 프림)
- * 2. 컴퓨터의 수 N은 1이상 1_000이하이다.
- * 3. 연결 선의 수 M은 1이상 100_000이하이다.
- * 4. 비용 c는 1이상 10_000이하이다.
- * 5. a와 b가 동일한 경우도 존재한다. (자기 자신)
+ * 1. 컴퓨터의 수 N은 1이상 1,000이하이다.
+ * 2. 연결할 수 있는 선의 수 1이상 100,000이하이다.
  * 
  * [풀이]
- * 1. 크루스칼 알고리즘을 적용하여 최소 비용을 계산한다.
+ * 1. 프림 알고리즘을 활용한다.
  */
 public class Main {
 	
+	private static int result;
 	private static int countOfComputers;
 	private static int countOfEdges;
-	private static int[] parents;
-	private static int[][] computers;
-	private static int result;
-	
-	private static BufferedReader br;
-	private static StringTokenizer st;
+	private static List<Edge>[] graph;
+	private static boolean[] isVisited;
 	
 	public static void main(String[] args) throws IOException {
 		init();
-		findResult();
+		prim();
 		System.out.println(result);
 	}
 	
 	/**
-	 * 결과를 도출하는 메서드.
+	 * 프림 알고리즘 메서드. 
 	 */
-	private static void findResult() {
-		int from, to, weight, fromParent, toParent;
-		for (int[] computer : computers) {
-			from = computer[0];
-			to = computer[1];
-			weight = computer[2];
+	private static void prim() {
+		PriorityQueue<Edge> pq = new PriorityQueue<>();
+		pq.offer(new Edge(1, 0)); // 시작 정점
+		
+		int vertex, weight;
+		while (!pq.isEmpty()) {
+			Edge edge = pq.poll();
+			vertex = edge.vertex;
+			weight = edge.weight;
 			
-			fromParent = find(from);
-			toParent = find(to);
-			if (fromParent != toParent) {
-				union(fromParent, toParent);
-				result += weight;
+			if (isVisited[vertex]) {
+				continue;
+			}
+			
+			isVisited[vertex] = true;
+			result += weight;
+			
+			for (Edge next : graph[vertex]) {
+				if (!isVisited[next.vertex]) {
+					pq.offer(next);
+				}
 			}
 		}
 	}
- 	
+	
 	/**
 	 * 초기 세팅을 진행하는 메서드.
 	 */
 	private static void init() throws IOException {
-		br = new BufferedReader(new InputStreamReader(System.in));
+		result = 0;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		countOfComputers = Integer.parseInt(br.readLine());
 		countOfEdges = Integer.parseInt(br.readLine());
-		parents = new int[countOfComputers + 1];
-		for (int index = 0; index < countOfComputers; index++) {
-			parents[index] = index;
+		graph = new ArrayList[countOfComputers + 1];
+		for (int index = 1; index <= countOfComputers; index++) {
+			graph[index] = new ArrayList<>();
 		}
-		computers = new int[countOfEdges][3]; // from, to, weight
+		isVisited = new boolean[countOfComputers + 1];
 		
+		StringTokenizer st;
+		int cur, next, weight;
 		for (int index = 0; index < countOfEdges; index++) {
 			st = new StringTokenizer(br.readLine());
-			computers[index][0] = Integer.parseInt(st.nextToken());
-			computers[index][1] = Integer.parseInt(st.nextToken());
-			computers[index][2] = Integer.parseInt(st.nextToken());
+			cur = Integer.parseInt(st.nextToken());
+			next = Integer.parseInt(st.nextToken());
+			weight = Integer.parseInt(st.nextToken());
+			graph[cur].add(new Edge(next, weight));
+			graph[next].add(new Edge(cur, weight));
 		}
-		Arrays.sort(computers, (o1, o2) -> o1[2] - o2[2]);
 	}
 	
-	private static void union(int aParent, int bParent) {
-		parents[bParent] = aParent;
-	}
-	
-	private static int find(int num) {
-		if (parents[num] == num) {
-			return num;
+	/**
+	 * 간선 정보를 담은 클래스.
+	 * 가중치를 기준으로 오름차순 정렬한다.
+	 */
+	static class Edge implements Comparable<Edge> {
+		int vertex;
+		int weight;
+		
+		Edge(int vertex, int weight) {
+			this.vertex = vertex;
+			this.weight = weight;
 		}
-		return parents[num] = find(parents[num]);
+
+		@Override
+		public int compareTo(Edge o) {
+			return this.weight - o.weight;
+		}
 	}
 }

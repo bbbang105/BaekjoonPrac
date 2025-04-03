6294a7deb128e5f8643611584f0d6ff560beef80
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-04-03 13:34:56 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 91 ms, Memory: 25,984 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -11,19 +11,18 @@ import java.util.StringTokenizer;
  * 4. 각 이용권의 요금과 각 달의 이용 계획이 주어질 때, 가장 적은 비용으로 수영장을 이용할 수 있는 방법을 찾아 그 값을 출력하라.
  * 
  * [풀이]
- * 1. 첫번째 인덱스부터 탐색을 시작한다.
- * 2. 이용 계획이 없다면 건너뛴다.
- * 3. 1일권, 1달권, 3달권을 사용하는 경우로 탐색한다.
- * 4. 인덱스가 12이상이라면 최소값을 비교 후 갱신한다. (11, 12월에도 3달권을 구입할 수 있다.)
- * *
+ * 1. dp[i] = i월까지의 최소 비용으로 정의한다.
+ * 2. 각 월마다 1일권, 1달권, 3달권을 비교하여 최소 비용을 갱신한다.
+ * 3. 마지막에 1년권과 비교하여 최소값을 출력한다.
  */
 public class Solution {
 
     private static BufferedReader br;
     private static StringTokenizer st;
+
     private static int[] monthlyPlans;
+    private static int[] dp;
     private static int oneDayFee, oneMonthFee, threeMonthFee, oneYearFee;
-    private static int minimumFee;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -32,9 +31,7 @@ public class Solution {
 
         for (int testCase = 1; testCase <= testCaseNum; testCase++) {
             init();
-            minimumFee = oneYearFee;
-            findMinimumFee(1, 0);
-            sb.append("#").append(testCase).append(" ").append(minimumFee).append('\n');
+            sb.append("#").append(testCase).append(" ").append(findMinimumFee()).append('\n');
         }
         System.out.print(sb);
     }
@@ -50,6 +47,8 @@ public class Solution {
         oneYearFee = Integer.parseInt(st.nextToken());
 
         monthlyPlans = new int[13];
+        dp = new int[13];
+
         st = new StringTokenizer(br.readLine());
         for (int index = 1; index <= 12; index++) {
             monthlyPlans[index] = Integer.parseInt(st.nextToken());
@@ -57,27 +56,21 @@ public class Solution {
     }
 
     /**
-     * 백트래킹을 활용하여 최소 비용을 찾는 메서드.
+     * DP를 활용하여 최소 비용을 구하는 메서드.
      */
-    private static void findMinimumFee(int index, int sum) {
-        if (index > 12) {
-            minimumFee = Math.min(minimumFee, sum);
-            return;
+    private static int findMinimumFee() {
+        for (int i = 1; i <= 12; i++) {
+            // 1일권
+            int dayFee = dp[i - 1] + monthlyPlans[i] * oneDayFee;
+
+            // 1달권
+            int monthFee = dp[i - 1] + oneMonthFee;
+
+            // 3달권
+            int threeFee = (i >= 3 ? dp[i - 3] : 0) + threeMonthFee;
+
+            dp[i] = Math.min(dayFee, Math.min(monthFee, threeFee)); // 
         }
-
-        // 이용 계획이 없으면 건너뜀
-        if (monthlyPlans[index] == 0) {
-            findMinimumFee(index + 1, sum);
-            return;
-        }
-
-        // 1일권 구입
-        findMinimumFee(index + 1, sum + (monthlyPlans[index] * oneDayFee));
-
-        // 1달권 구입
-        findMinimumFee(index + 1, sum + oneMonthFee);
-
-        // 3달권 구입
-        findMinimumFee(index + 3, sum + threeMonthFee);
+        return Math.min(dp[12], oneYearFee);
     }
 }

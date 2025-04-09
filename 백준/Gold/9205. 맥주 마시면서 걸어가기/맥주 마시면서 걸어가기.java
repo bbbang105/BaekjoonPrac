c643a4cfcb495704393ddbc1ce04c53887d1df57
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-04-09 16:07:30 +0900

    [Gold V] Title: 맥주 마시면서 걸어가기, Time: 80 ms, Memory: 12448 KB -BaekjoonHub

--- 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
+++ 백준/Gold/9205. 맥주 마시면서 걸어가기/맥주 마시면서 걸어가기.java
@@ -1,88 +1,119 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
+/**
+ * [조건 및 풀이]
+ * 1. 맥주는 최대 20개 까지만 소지 가능하다 (1,000 초과의 거리는 이동할 수 없다.)
+ * 2. 편의점에 방문하면 맥주를 다시 채울 수 있다. (20개를 모두 구입하는 것으로 한다.)
+ * 3. 거리는 맨해튼 거리로 계산한다.
+ * 4. 상근이네 집(시작점) / 편의점 위치들 / 페스티벌 위치(도착점) 좌표가 주어진다.
+ * 5. 도착할 수 있는지에 따라 결과를 출력하라.
+ * 6. 도착지도 편의점 배열 마지막에 넣어줌으로서 방문할 수 있게 만든다.
+ */
 public class Main {
-    private static final int MAX_MOVEMENT_DISTANCE = 1_000;
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-
-        while (T-- > 0) {
-            int N = Integer.parseInt(br.readLine());
-
-            st = new StringTokenizer(br.readLine());
-            int sx = Integer.parseInt(st.nextToken());
-            int sy = Integer.parseInt(st.nextToken());
-            Node start = new Node(sx, sy);
-
-            List<Node> conveniences = new ArrayList<>();
-            for (int n = 0; n < N; n++) {
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                conveniences.add(new Node(x, y));
-            }
-
-            st = new StringTokenizer(br.readLine());
-            int ex = Integer.parseInt(st.nextToken());
-            int ey = Integer.parseInt(st.nextToken());
-            Node end = new Node(ex, ey);
-
-            if (bfs(start, end, conveniences)) {
-                sb.append("happy").append('\n');
-            } else {
-                sb.append("sad").append('\n');
-            }
-        }
-
-        System.out.print(sb);
-    }
-
-    private static boolean bfs(Node start, Node end, List<Node> conveniences) {
-        boolean[] visited = new boolean[conveniences.size()];
-        int ex = end.x;
-        int ey = end.y;
-
-        Queue<Node> q = new LinkedList<>();
-        q.offer(start);
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            int x = cur.x;
-            int y = cur.y;
-            if (calculateDistance(x, y, ex, ey) <= MAX_MOVEMENT_DISTANCE) {
-                return true;
-            }
-
-            for (int i = 0; i < conveniences.size(); i++) {
-                Node convenience = conveniences.get(i);
-                int cx = convenience.x;
-                int cy = convenience.y;
-                if (!visited[i] && calculateDistance(x, y, cx, cy) <= MAX_MOVEMENT_DISTANCE) {
-                    visited[i] = true;
-                    q.offer(new Node(cx, cy));
-                }
-            }
-        }
-
-        return false;
-    }
-
-    private static int calculateDistance(int a, int b, int c, int d) {
-        return (Math.abs((c - a)) + Math.abs((d - b)));
-    }
-
-    static class Node {
-        int x;
-        int y;
-
-        Node(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-    }
+	
+	private static final int LIMIT = 1_000; // 1_000 초과 거리는 이동 불가능
+	
+	private static String result;
+	private static Pos[] stores;
+	private static boolean[] isVisited;
+	private static Queue<Pos> q;
+	private static int endX;
+	private static int endY;
+	
+	private static BufferedReader br;
+	private static StringTokenizer st;
+	
+	/**
+	 * 메인 메서드.
+	 */
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+		br = new BufferedReader(new InputStreamReader(System.in));
+		int testCaseNum = Integer.parseInt(br.readLine());
+		for (int testCase = 1; testCase <= testCaseNum; testCase++) {
+			init();
+			findResult();
+			sb.append(result).append('\n');
+		}
+		System.out.println(sb);
+	}
+	
+	/**
+	 * 결과를 도출하는 메서드.
+	 */
+	private static void findResult() {
+		Pos cur;
+		int x, y;
+		while (!q.isEmpty()) {
+			cur = q.poll();
+			x = cur.x;
+			y = cur.y;
+			if (x == endX && y == endY) {
+				result = "happy";
+				break;
+			}
+			
+			Pos store;
+			for (int index = 0; index < stores.length; index++) {
+				store = stores[index];
+				if (canMove(x, store.x, y, store.y, index)) {
+					isVisited[index] = true;
+					q.offer(store);
+				}
+			}
+		}
+	}
+	
+	/**
+	 * 맨해튼 거리를 기반으로, 이동 가능 여부를 반환하는 메서드.
+	 */
+	private static boolean canMove(int x, int nx, int y, int ny, int index) {
+		return Math.abs(nx - x) + Math.abs(ny - y) <= LIMIT && !isVisited[index];
+	}
+	
+	/**
+	 * 초기 세팅 메서드.
+	 */
+	private static void init() throws IOException {
+		result = "sad";
+		int counfOfStores = Integer.parseInt(br.readLine());
+		stores = new Pos[counfOfStores + 1];
+		isVisited = new boolean[counfOfStores + 1];
+		q = new LinkedList<>();
+		
+		st = new StringTokenizer(br.readLine());
+		int startX = Integer.parseInt(st.nextToken());
+		int startY = Integer.parseInt(st.nextToken());
+		q.offer(new Pos(startX, startY));
+		
+		for (int index = 0; index < counfOfStores; index++) {
+			st = new StringTokenizer(br.readLine());
+			int storeX = Integer.parseInt(st.nextToken());
+			int storeY = Integer.parseInt(st.nextToken());
+			stores[index] = new Pos(storeX, storeY);
+		}
+		
+		st = new StringTokenizer(br.readLine());
+		endX = Integer.parseInt(st.nextToken());
+		endY = Integer.parseInt(st.nextToken());
+		stores[counfOfStores] = new Pos(endX, endY); // 도착지도 함께 넣어줌
+	}
+	
+	/**
+	 * 좌표의 정보를 담은 클래스.
+	 */
+	static class Pos {
+		int x;
+		int y;
+		
+		Pos(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 }

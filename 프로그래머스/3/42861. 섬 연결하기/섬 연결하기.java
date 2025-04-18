794812580b1e555bd40a206ae9a758877a2df974
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-04-18 16:26:39 +0900

    [level 3] Title: 섬 연결하기, Time: 1.80 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,46 +1,41 @@
 import java.util.*;
+import java.io.*;
 
 class Solution {
     
     private static int[] parents;
     
     public int solution(int n, int[][] costs) {
+        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
         int answer = 0;
         parents = new int[n];
         for (int index = 0; index < n; index++) {
             parents[index] = index;
         }
         
-        Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
-        
         int a, b, w;
+        int aRoot, bRoot;
         for (int[] cost : costs) {
             a = cost[0];
             b = cost[1];
             w = cost[2];
             
-            int aParent = find(a);
-            int bParent = find(b);
-            if (aParent != bParent) {
+            aRoot = find(a);
+            bRoot = find(b);
+            if (aRoot != bRoot) {
+                union(aRoot, bRoot);
                 answer += w;
-                parents[bParent] = aParent;
             }
         }
-        
         return answer;
     }
     
-    /*
-    private static void union(int a, int b) {
-        int aParent = find(a);
-        int bParent = find(b);
-        if (aParent != bParent) {
-            parents[bParent] = aParent;
-        }
-    }*/
+    private static void union(int aRoot, int bRoot) {
+        parents[bRoot] = aRoot;
+    }
     
     private static int find(int num) {
-        if (parents[num] == num) {
+        if (num == parents[num]) {
             return num;
         }
         return parents[num] = find(parents[num]);

c2c7cb432b1bc0fe376e97445190333510ff69fa
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-04-18 17:14:39 +0900

    [level 3] Title: 단속카메라, Time: 9.35 ms, Memory: 60.6 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -2,17 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] routes) {
-        Arrays.sort(routes, (o1, o2) -> o1[1] - o2[1]);
         int answer = 1;
-        int len = routes.length;
-        int maxNum = routes[0][1];
+        Arrays.sort(routes, (o1, o2) -> o1[1] - o2[1]);
         
-        for (int i = 1; i < len; i++) {
-            if (maxNum >= routes[i][0] && maxNum <= routes[i][1]) continue;
-            maxNum = routes[i][1];
+        int lastLocation = routes[0][1];
+        for (int index = 1; index < routes.length; index++) {
+            if (routes[index][0] <= lastLocation && routes[index][1] >= lastLocation) {
+                continue;
+            }
+            lastLocation = routes[index][1];
             answer++;
         }
-        
         return answer;
     }
 }

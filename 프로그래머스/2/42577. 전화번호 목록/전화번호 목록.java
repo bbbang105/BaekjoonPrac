173228e97a971683bbe023289189cb3e574a48d3
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-06-25 18:23:51 +0900

    [level 2] Title: 전화번호 목록, Time: 375.90 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -1,18 +1,28 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
-        boolean answer = true;
         Arrays.sort(phone_book);
-
-        int len = phone_book.length;
-        for (int i = 0; i < len-1; i++) {
-            if (phone_book[i+1].startsWith(phone_book[i])) {
+        
+        boolean answer = true;
+        String cur = "";
+        String next = "";
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            cur = phone_book[i];
+            next = phone_book[i + 1];
+            boolean isHead = true;
+            for (int k = 0; k < cur.length(); k++) {
+                if (cur.charAt(k) != next.charAt(k)) {
+                    isHead = false;
+                    break;
+                }
+            }
+            
+            if (isHead) {
                 answer = false;
                 break;
             }
         }
-
         return answer;
     }
 }

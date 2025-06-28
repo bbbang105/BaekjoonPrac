1d905e2ffbd19b9a0b79a09f18e82ac90b7b125a
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-06-28 13:39:39 +0900

    [level 3] Title: 부대복귀, Time: 259.09 ms, Memory: 195 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,51 +1,55 @@
 import java.util.*;
 
 class Solution {
-    private static List<List<Integer>> graph; // 간선 정보
+    
+    private static Map<Integer, List<Integer>> graph; // 간선 정보
     private static int[] dist; // 최단 거리
     private static int start; // 시작점
-    private static final int MAX_VALUE = Integer.MAX_VALUE;
+    private static final int INF = Integer.MAX_VALUE;
     
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
         int[] result = new int[sources.length];
         dist = new int[n + 1];
         start = destination;
         
-        graph = new ArrayList<>();
-        for (int i = 0; i < n + 1; i++) graph.add(new ArrayList<>());
-        
+        graph = new HashMap<>();
+        for (int i = 1; i <= n; i++) {
+            graph.put(i, new ArrayList<>());
+        }
+        int a, b;
         for (int[] road : roads) {
+            a = road[0];
+            b = road[1];
             // 양방향
-            graph.get(road[0]).add(road[1]);
-            graph.get(road[1]).add(road[0]);
+            graph.get(a).add(b);
+            graph.get(b).add(a);
         }
         
-        // 시작 설정 후 다익스트라
-        Arrays.fill(dist, MAX_VALUE);
+        Arrays.fill(dist, INF);
         dist[start] = 0;
         dijkstra();
         
         for (int i = 0; i < sources.length; i++) {
-            if (dist[sources[i]] < MAX_VALUE) {
+            if (dist[sources[i]] < INF) {
                 result[i] = dist[sources[i]];
-            } else result[i] = -1;
-        } 
+            } else {
+                result[i] = -1;
+            }
+        }
         
         return result;
     }
     
-    private void dijkstra() {
+    private static void dijkstra() {
         Queue<Integer> q = new LinkedList<>();
         q.offer(start);
         
-        while (!q.isEmpty()) {
-            int n = q.poll();
+        while(!q.isEmpty()) {
+            int cur = q.poll();
             
-            for (int i = 0; i < graph.get(n).size(); i++) {
-                int next = graph.get(n).get(i);
-                // start -> n -> next가 start -> next 보다 더 빠른 경우, 경로 압축
-                if (dist[n] + 1 < dist[next]) {
-                    dist[next] = dist[n] + 1;
+            for (int next : graph.get(cur)) {
+                if (dist[cur] + 1 < dist[next]) {
+                    dist[next] = dist[cur] + 1;
                     q.offer(next);
                 }
             }

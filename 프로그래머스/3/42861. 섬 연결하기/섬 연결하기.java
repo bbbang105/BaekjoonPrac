b6dff84cfc18516b654e86d2652f746f938aa610
Sangho Han <113084292+bbbang105@users.noreply.github.com>
2025-06-28 13:48:23 +0900

    [level 3] Title: 섬 연결하기, Time: 0.79 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.io.*;
 
 class Solution {
     
@@ -7,31 +6,32 @@ class Solution {
     
     public int solution(int n, int[][] costs) {
         Arrays.sort(costs, (o1, o2) -> o1[2] - o2[2]);
-        int answer = 0;
+        int result = 0;
         parents = new int[n];
-        for (int index = 0; index < n; index++) {
-            parents[index] = index;
+        for (int i = 0; i < n; i++) {
+            parents[i] = i;
         }
         
         int a, b, w;
-        int aRoot, bRoot;
+        int aParent, bParent;
         for (int[] cost : costs) {
             a = cost[0];
             b = cost[1];
             w = cost[2];
             
-            aRoot = find(a);
-            bRoot = find(b);
-            if (aRoot != bRoot) {
-                union(aRoot, bRoot);
-                answer += w;
+            aParent = find(a);
+            bParent = find(b);
+            if (aParent != bParent) {
+                union(aParent, bParent);
+                result += w;
             }
         }
-        return answer;
+        
+        return result;
     }
     
-    private static void union(int aRoot, int bRoot) {
-        parents[bRoot] = aRoot;
+    private static void union(int aParent, int bParent) {
+        parents[bParent] = aParent;
     }
     
     private static int find(int num) {
